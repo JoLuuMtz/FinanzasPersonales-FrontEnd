4596d10511cfb1e42312168706aa400d75635d3c
JoLu Martinez <jorgemartinezricardo@gmail.com>
2025-09-17 23:51:39 -0500

    PforileCard: Nombres del usuario, y input file para la foto del usuario

--- src/app/dashboard/pages/welcome-page/welcome-page.component.ts
+++ src/app/dashboard/pages/welcome-page/welcome-page.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, computed, inject, OnInit } from '@angular/core';
 
 import { TitleComponent } from '../../components/title/title.component';
 import { ProfileCardComponent } from '../../components/profile-card/profile-card.component';
+import { UserService } from '../../../user/services/user.service';
 
 @Component({
   selector: 'app-welcome-page',
@@ -9,7 +10,11 @@ import { ProfileCardComponent } from '../../components/profile-card/profile-card
   templateUrl: './welcome-page.component.html',
 })
 export class WelcomePageComponent {
+  private readonly _userService = inject(UserService);
+  public user = computed(() => this._userService.User());
 
-
-
+  public name = this.user()?.name.toString();
+  public lastname = this.user()?.lastName.toString();
+  public email = this.user()?.email.toString();
+  public profileImage = this.user()?.profileImagen;
 }
